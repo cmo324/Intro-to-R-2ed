351f6f5c6a013c5473c0b690bfd03ef22eefdea0
cmo324 <cmo324@lehigh.edu>
2022-02-09 21:39:11 -0500

    Update Intro to R.R
    Callie O'Neill

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for CallieONeill 2019
+# Copy for CallieOeill 2019
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
